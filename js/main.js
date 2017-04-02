05253372e34d1eb161555f96faa9f710ee49cfc2
Michael Fouquet <michael.fouquet@hudl.com>
2017-04-02 10:19:41 -0500

    Begin work on getting the quotes added.

--- js/main.js
+++ js/main.js
@@ -2,45 +2,128 @@ $(document).ready(function() {
   $('#settingsNudge').focus().val('1');
 
   $(document).keydown(function(e) {
+    var nudge = $('#settingsNudge').val();
+    var push = $('#settingsPush').val();
+    var shove = $('#settingsShove').val();
+
+    if (e.which === 38 && e.altKey && e.shiftKey) {
+      $('#poo').css({top: '-=' + shove + 'px'});
+      var number = Math.floor(Math.random() * pooQuotes.length);
+      $('#poo .try__emoji-bubble').html(pooQuotes[number]);
+      e.preventDefault();
+      return;
+    }
+
+    if (e.which === 40 && e.altKey && e.shiftKey) {
+      $('#poo').css({top: '+=' + shove + 'px'});
+      var number = Math.floor(Math.random() * pooQuotes.length);
+      $('#poo .try__emoji-bubble').html(pooQuotes[number]);
+      e.preventDefault();
+      return;
+    }
+
+    if (e.which === 37 && e.altKey && e.shiftKey) {
+      $('#poo').css({left: '-=' + shove + 'px'});
+      var number = Math.floor(Math.random() * pooQuotes.length);
+      $('#poo .try__emoji-bubble').html(pooQuotes[number]);
+      e.preventDefault();
+      return;
+    }
+
+    if (e.which === 39 && e.altKey && e.shiftKey) {
+      $('#poo').css({left: '+=' + shove + 'px'});
+      var number = Math.floor(Math.random() * pooQuotes.length);
+      $('#poo .try__emoji-bubble').html(pooQuotes[number]);
+      e.preventDefault();
+      return;
+    }
+
+
+    if (e.which === 38 && e.shiftKey) {
+      $('#alien').css({top: '-=' + push + 'px'});
+      var number = Math.floor(Math.random() * alienQuotes.length);
+      $('#alien .try__emoji-bubble').html(alienQuotes[number]);
+      e.preventDefault();
+      return;
+    }
+
+    if (e.which === 40 && e.shiftKey) {
+      $('#alien').css({top: '+=' + push + 'px'});
+      var number = Math.floor(Math.random() * alienQuotes.length);
+      $('#alien .try__emoji-bubble').html(alienQuotes[number]);
+      e.preventDefault();
+      return;
+    }
+
+    if (e.which === 37 && e.shiftKey) {
+      $('#alien').css({left: '-=' + push + 'px'});
+      var number = Math.floor(Math.random() * alienQuotes.length);
+      $('#alien .try__emoji-bubble').html(alienQuotes[number]);
+      e.preventDefault();
+      return;
+    }
+
+    if (e.which === 39 && e.shiftKey) {
+      $('#alien').css({left: '+=' + push + 'px'});
+      var number = Math.floor(Math.random() * alienQuotes.length);
+      $('#alien .try__emoji-bubble').html(alienQuotes[number]);
+      e.preventDefault();
+      return;
+    }
+
     switch(e.which) {
       case 38: // up
       $('.up').addClass('pressed');
+      $('#robot').css({top: '-=' + nudge + 'px'});
+      var number = Math.floor(Math.random() * robotQuotes.length);
+      $('#robot .try__emoji-bubble').html(robotQuotes[number]);
       break;
 
       case 40: // down
       $('.down').addClass('pressed');
+      $('#robot').css({top: '+=' + nudge + 'px'});
+      var number = Math.floor(Math.random() * robotQuotes.length);
+      $('#robot .try__emoji-bubble').html(robotQuotes[number]);
       break;
 
       case 37: // left
       $('.left').addClass('pressed');
+      $('#robot').css({left: '-=' + nudge + 'px'});
+      var number = Math.floor(Math.random() * robotQuotes.length);
+      $('#robot .try__emoji-bubble').html(robotQuotes[number]);
       break;
 
       case 39: // right
       $('.right').addClass('pressed');
+      $('#robot').css({left: '+=' + nudge + 'px'});
+      var number = Math.floor(Math.random() * robotQuotes.length);
+      $('#robot .try__emoji-bubble').html(robotQuotes[number]);
       break;
 
-      case 78: // right
+      case 78: // n
       $('.n').addClass('pressed');
       break;
 
-      case 16: // right
+      case 16: // shift
       $('.shift').addClass('pressed');
       break;
 
-      case 17: // right
+      case 17: // ctrl
       $('.ctrl').addClass('pressed');
       break;
 
-      case 18: // right
+      case 18: // option
       $('.option').addClass('pressed');
       break;
 
-      case 91: // right
+      case 91: // cmd
       $('.cmd').addClass('pressed');
       break;
 
       default: return; // exit this handler for other keys
     }
+
+    e.preventDefault();
   });
 
   $(document).keyup(function(e) {
@@ -61,23 +144,23 @@ $(document).ready(function() {
       $('.right').removeClass('pressed');
       break;
 
-      case 78: // right
+      case 78: // n
       $('.n').removeClass('pressed');
       break;
 
-      case 16: // right
+      case 16: // shift
       $('.shift').removeClass('pressed');
       break;
 
-      case 17: // right
+      case 17: // ctrl
       $('.ctrl').removeClass('pressed');
       break;
 
-      case 18: // right
+      case 18: // option
       $('.option').removeClass('pressed');
       break;
 
-      case 91: // right
+      case 91: // cmd
       $('.cmd').removeClass('pressed');
       break;
 
