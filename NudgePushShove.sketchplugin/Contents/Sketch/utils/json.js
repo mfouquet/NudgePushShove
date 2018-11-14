773892ab8e03e448e4a21a776ddf6d8194c9e9ac
Michael Fouquet <michael.fouquet@me.com>
2018-11-13 21:30:53 -0600

    Revert "initial commit from skpm"
    This reverts commit 8f3569a814e96efcf625c9968ab1f88eee9442fc.

---
+++ NudgePushShove.sketchplugin/Contents/Sketch/utils/json.js
@@ -0,0 +1,28 @@
+var saveJsonToFile = function(context, jsonObject, filePath) {
+  var scriptPath = context.scriptPath;
+  var scriptFolder = [scriptPath stringByDeletingLastPathComponent];
+
+  writeTextToFile(stringify(jsonObject), scriptFolder + filePath);
+}
+
+var stringify = function(obj, prettyPrinted) {
+  var prettySetting = prettyPrinted ? NSJSONWritingPrettyPrinted : 0,
+  jsonData = [NSJSONSerialization dataWithJSONObject:obj options:prettySetting error:nil];
+  return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
+
+var writeTextToFile = function(text, filePath) {
+  var t = [NSString stringWithFormat:@"%@", text],
+  f = [NSString stringWithFormat:@"%@", filePath];
+  return [t writeToFile:f atomically:true encoding:NSUTF8StringEncoding error:nil];
+}
+
+var jsonFromFile = function(filePath, mutable) {
+  var data = [NSData dataWithContentsOfFile:filePath];
+  if (!data) {
+    return null;
+  }
+
+  var options = mutable == true ? NSJSONReadingMutableContainers : 0
+  return [NSJSONSerialization JSONObjectWithData:data options:options error:nil];
+}
---
+++ NudgePushShove.sketchplugin/Contents/Sketch/utils/threading.js
@@ -0,0 +1,3 @@
+var mainThreadDict = NSThread.mainThread().threadDictionary();
+var kPluginDomain = "com.mfouquet.sketch.nudgePushShove";
+var kShoveAmount = kPluginDomain + ".shoveAmount";
